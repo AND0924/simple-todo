87c157580dd6484963f6d3079915d27ac948e64d
AND0924 <mimimiarioka1130@gmail.com>
2020-10-15 17:02:18 +0900

    Updates

---
+++ precache-manifest.6a57847949cb1653a23611589b3c0796.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c41bb7108ae60f8c1e0abf339af952",
+    "url": "/simple-todo/index.html"
+  },
+  {
+    "revision": "b3bf2b6a3a003a129338",
+    "url": "/simple-todo/static/css/main.39eff0a4.chunk.css"
+  },
+  {
+    "revision": "48743d0ba2832778c5fa",
+    "url": "/simple-todo/static/js/2.fd19c48f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-todo/static/js/2.fd19c48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3bf2b6a3a003a129338",
+    "url": "/simple-todo/static/js/main.68720493.chunk.js"
+  },
+  {
+    "revision": "a9b828f3067ebb10d15b",
+    "url": "/simple-todo/static/js/runtime-main.b107b839.js"
+  }
+]);
